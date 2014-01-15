17f05b86d2379ab881cc603933ffad74e3d1c4eb
Jordan Luft <jorluft@gmail.com>
2014-01-14 17:32:14 -0800

    created dmat app

---
+++ dmat/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dmat::Application.config.secret_key_base = '036d1659915d277f942ff04e03839dab43266618259d8d4585e5d28adb541f83f2ceb5d8c8f65a919d33e9a00dd810d5d71506ef760ee096cc6ab314dbdc5519'
